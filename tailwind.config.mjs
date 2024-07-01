116157b07893636013386720660dd58b90d680ef
Narek Pogosian <narekpogosian2@gmail.com>
2024-07-01 18:52:33 +0200

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      serif: ["Merriweather", ...defaultTheme.fontFamily.sans],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        element: "hsl(var(--element))",
+        hover: "hsla(var(--hover))",
+        "muted-foreground": "hsl(var(--muted-foreground))",
+      },
+    },
+  },
+  plugins: [],
+};
